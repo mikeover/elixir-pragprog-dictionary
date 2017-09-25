99b9c1a6c3b31c1cb1f5e291ee1226d33d27f32e
Mike Over <mike.over@teamsnap.com>
2017-09-25 16:45:38 -0400

    Make the dictionary use Agents

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,13 +1,18 @@
 defmodule Dictionary.WordList do
-  def word_list do
+  
+  def start_link do
+    Agent.start_link(&word_list/0)
+  end
+
+  def random_word(agent) do
+    agent
+    |> Agent.get(&Enum.random/1)
+  end
+
+  defp word_list do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
     |> File.read!
     |> String.split(~r/\n/)
-  end
-
-  def random_word(words) do
-    words
-    |> Enum.random
   end
 end
